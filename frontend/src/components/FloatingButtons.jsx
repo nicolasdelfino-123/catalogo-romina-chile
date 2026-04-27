38a5612565f1fa3fb76bb1f8c58f766b7b48336c
Nicolas <nicolasdelfino585@gmail.com>
2026-04-27 08:35:33 -0300

    se saco icono flotante wapp y se cambio texto index por nuevo texto sugerido por clienta

--- frontend/src/components/FloatingButtons.jsx
+++ frontend/src/components/FloatingButtons.jsx
@@ -69,7 +69,7 @@ export default function FloatingButtons() {
                 </a>
 
                 {/* WhatsApp */}
-                <a
+                {/*  <a
                     href={WA_URL}
                     target="_blank"
                     rel="noopener noreferrer"
@@ -86,13 +86,13 @@ export default function FloatingButtons() {
             outline-none focus:ring-2 focus:ring-white/50
           "
                 >
-                    {/* ícono simple (SVG) */}
+             
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                         className="w-6 h-6 sm:w-7 sm:h-7 text-white">
                         <path fill="currentColor" d="M20.5 3.5A10.5 10.5 0 0 0 4 17.1L3 21l4-1a10.5 10.5 0 0 0 16.5-8.5A10.4 10.4 0 0 0 20.5 3.5m-8.5 16a8.5 8.5 0 0 1-4.3-1.2l-.3-.2l-2.5.6l.7-2.4l-.2-.3A8.5 8.5 0 1 1 12 19.5m4.8-6.1c-.3-.1-1.7-.8-1.9-.9s-.4-.1-.6.1s-.7.9-.9 1.1s-.3.2-.6.1s-1.1-.4-2.1-1.3A7.6 7.6 0 0 1 9 10.7c-.2-.3 0-.4.1-.6l.3-.4l.2-.3c.1-.1.1-.2.2-.3s0-.3 0-.4s0-.3-.1-.4s-.6-1.5-.8-2s-.4-.5-.6-.5h-.5a1 1 0 0 0-.7.3a3 3 0 0 0-1 2.3a5.2 5.2 0 0 0 1.1 3.1a11.9 11.9 0 0 0 4.6 4.1a5.1 5.1 0 0 0 3.2 1h.7a2.8 2.8 0 0 0 2-1.3a2.3 2.3 0 0 0 .2-1.3s-.2-.1-.5-.2" />
                     </svg>
 
-                    {/* Tooltip */}
+     
                     <span className="
             absolute right-full mr-2 hidden sm:block
             whitespace-nowrap
@@ -104,7 +104,7 @@ export default function FloatingButtons() {
           ">
                         WhatsApp
                     </span>
-                </a>
+                </a> */}
             </div>
         </>
     );
